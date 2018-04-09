329657261f7ff7ca75a4378822733163c859a5aa
bshortridge <bshortridge@gmail.com>
2018-04-09 12:07:10 +0100

    Added function for converting string to laptime in milliseconds

--- tmp/main.cpp
+++ tmp/main.cpp
@@ -21,6 +21,43 @@
 /*
  * 
  */
+unsigned long timeStringToMs (std::string timeString)
+{
+  unsigned long timeInMs;
+  std::string hourString ("0"), minuteString ("0"), secondString ("0");
+  std::string millisecondString ("0");
+  std::size_t startIdx = 0;
+  std::size_t colonPos = timeString.find (":");
+  std::size_t decimalPos = timeString.find (".");
+  if (std::string::npos == decimalPos)
+  {
+    hourString = timeString.substr (startIdx, colonPos);
+    startIdx = colonPos + 1;
+    colonPos = timeString.find (":", startIdx);
+    decimalPos = timeString.length ();
+  }
+  
+  minuteString = timeString.substr (startIdx, colonPos - startIdx);
+  secondString = timeString.substr (colonPos + 1, decimalPos - colonPos);
+  if (decimalPos != timeString.length ())
+  {
+    millisecondString = timeString.substr (decimalPos + 1);
+  }
+  
+  int hours = std::stoi (hourString);
+  int minutes = std::stoi (minuteString);
+  int seconds = std::stoi (secondString);
+  int milliseconds = std::stoi (millisecondString);
+  
+  timeInMs = (hours * 60 * 60 * 1000) + (minutes * 60 * 1000)
+              + (seconds * 1000) + milliseconds;
+  
+  std::cout << hours << " hours, " << minutes << " minutes, "
+    << seconds << " seconds, " << milliseconds << " milliseconds" << std::endl;
+  
+  return timeInMs;
+}
+
 int main (int argc, char** argv)
 {
   Json::Value root;
